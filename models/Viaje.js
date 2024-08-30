d99817a6d4e589774784dc1b49febceac50f538d
Jose A. Menacho Minchola <josmendev@gmail.com>
2024-08-30 00:06:25 -0500

    Creacion del modelo

---
+++ models/Viaje.js
@@ -0,0 +1,35 @@
+import Sequelize from "sequelize";
+import db from "../config/db.js";
+// importo mi conexion
+
+export const Viaje = db.define('viajes', {
+    // defino el nombre de mi tabla y sus campos
+    // nombre: viajes
+    // a continuacion sus campso
+    titulo: {
+        type: Sequelize.STRING
+    },
+    precio: {
+        type: Sequelize.STRING
+    },
+    fecha_ida: {
+        type: Sequelize.DATE
+    },
+    fecha_vuelta: {
+        type: Sequelize.DATE
+    },
+    imagen: {
+        type: Sequelize.STRING
+    },
+    descripcion: {
+        type: Sequelize.STRING
+    },
+    disponibles: {
+        type: Sequelize.INTEGER
+    },
+    eslug: {
+        type: Sequelize.STRING
+    }
+});
+
+
